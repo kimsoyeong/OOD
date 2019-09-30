1cd3fb60681a01f7c1faf0f20beb1fbfa3cddcc6
kimsoyeong <201802066@o.cnu.ac.kr>
2019-09-30 12:20:17 +0900

    5주차 실습

---
+++ OOD05/OOD05/실습1,2,3,4.cpp
@@ -0,0 +1,49 @@
+﻿
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+//실습 1,2,3
+int i;
+
+class A
+{
+public:
+	~A()
+	{
+		i = 10;
+	}
+};
+
+//실습4
+class Animal {
+public:
+	
+	Animal(int numberOfLeg) {
+		this->numberOfLeg = numberOfLeg;
+	}
+private:
+	int numberOfLeg;
+};
+
+int& foo() //실습2-reference
+{
+	i = 3;
+	{ //실습3-중괄호
+		A ob;
+	}
+	return i;
+}
+
+int main()
+{
+	cout << foo() << endl;
+
+	//실습 4번 답들
+	//Animal a;
+	Animal a(1);
+	//Animal *a = new Animal(1);
+	return 0;
+}
+
---
+++ OOD05/OOD05/실습5.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include <iostream>
+
+//실습5
+class Animal {
+public:
+	char* name;
+	int age;
+
+	Animal(int age_, const char* name_) {
+		age = age_;
+		name = new char[strlen(name_) + 1];
+		strcpy_s(name, strlen(name_) + 1, name_);
+	}
+
+	Animal(Animal& a) { //복사 생성자
+		age = a.age;
+
+		name = new char[strlen(a.name) + 1];
+		strcpy_s(name, strlen(a.name) + 1, a.name);
+		//name = a.name; //a.name의 주소값을 name에 넣어주는 것
+	}
+
+	void changeName(const char* newName) {
+		strcpy_s(name, strlen(newName) + 1, newName);
+	}
+	void printAnimal() {
+		std::cout << "Name: " << name << " Age: "
+			<< age << std::endl;
+	}
+};
+
+void main() {
+
+	Animal A(10, "Jenny"); //10살 Jenny 생성
+	Animal B = A; // 10살 Jenny인 A를 B에게 복사
+	A.age = 22;		//A의 나이를 22살로 바꿈
+	A.changeName("Brown"); //A의 이름을 Brown으로 바꿈
+
+	A.printAnimal();
+	B.printAnimal();
+}
