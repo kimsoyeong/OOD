df98d31d78aa04d94d0751934117b3bce234a540
kimsoyeong <201802066@o.cnu.ac.kr>
2019-11-11 11:38:18 +0900

    10주차 실습

---
+++ OOD10/OOD10/실습1.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <iostream>
+
+void swapInt(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void swapFloat(float& a, float& b) {
+	float temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+void swap(T& a, T&b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+int main() {
+	int a = 1;
+	int b = 2;
+	float c = 3.3;
+	float d = 4.4;
+
+	swap(a, b);
+	swap(c, d);
+/*	swapInt(a, b);
+	swapFloat(c, d); */
+
+	std::cout << "a: " << a << ", b: " << b << std::endl;
+	std::cout << "c: " << c << ", d: " << d << std::endl;
+
+	return 0;
+}
+
---
+++ OOD10/OOD10/실습2.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+/* ....(실습1)에서 구현된 template swap ....*/
+template <typename T>
+void swap(T& a, T&b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+/* ... (추가) template swap의 string type specialization ... */
+template<>
+void std::swap(std::string& a, std::string& b) {
+	//type 선언 형태는 위의 함수와 동일하게 해줘야 specialization 가능
+	std::string temp = a;
+	a = b;
+	b = temp;
+}
+
+int main() {
+	int a = 1;
+	int b = 2;
+	float c = 3.3;
+	float d = 4.4;
+	std::string e = "World";
+	std::string f = "Hello";
+
+	swap<int>(a, b); //swap(a,b)도 가능
+	swap<float>(c, d); //swap(c,d)도 가능
+
+   //(추가) e, f의 swap이 가능하도록 swap template 호출
+	swap<>(e, f);
+
+	std::cout << "a: " << a << ", b: " << b << std::endl;
+	std::cout << "c: " << c << ", d: " << d << std::endl;
+	std::cout << "e: " << e << ", f: " << f << std::endl;
+
+	return 0;
+}
